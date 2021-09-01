7615a7cf35d4fac7f9a4f510c39c449f7eae4a8c
JE <yoontae@JEui-MacBookPro.local>
2021-09-01 09:13:17 +0900

    prisma

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -5,7 +5,6 @@ import {
   OnModuleDestroy,
 } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
